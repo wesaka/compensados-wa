0ed60cedb292e16d93f442faa379d1af14a267f7
wesaka <kalatai.alberti@gmail.com>
2020-01-11 17:33:33 -0300

    Updated the contact form to be usable now

--- src/components/contato-form/contato-form.component.jsx
+++ src/components/contato-form/contato-form.component.jsx
@@ -1,19 +1,54 @@
-import React from "react";
+import React, {Component} from "react";
 import SectionTitleComponent from "../section-title/section-title.component";
 import {ButtonContato, FormContato, InputBig, InputSmall} from "./contato-form.styles";
 import {ContatoContainer} from "./contato-form.styles";
+import * as emailjs from "emailjs-com";
 
-const ContatoForm = () => (
-    <ContatoContainer>
-        <SectionTitleComponent title='Entre em contato'/>
-        <FormContato>
-            <InputSmall type='text' name='fname' placeholder='Nome (Obrigatório)'/>
-            <InputSmall type='email' name='email' placeholder='E-mail (Obrigatório)'/>
-            <InputSmall type='text' name='subject' placeholder='Assunto'/>
-            <InputBig type='text' name='message' placeholder='Mensagem'/>
-        </FormContato>
-        <ButtonContato>ENVIAR MENSAGEM</ButtonContato>
-    </ContatoContainer>
-);
+const sendEmail = ({name, email, subject, message}) => {
+    const templateParams = {
+        from_name: name,
+        email: email,
+        subject: subject,
+        message_html: message
+    };
+
+    emailjs.send(
+        'gmail',
+        'template_FeYGUoYg',
+        templateParams,
+        process.env.REACT_APP_EMAIL_KEY
+    )
+};
+
+class ContatoForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.inputName = React.createRef();
+        this.inputEmail = React.createRef();
+        this.inputSubject = React.createRef();
+        this.inputSubject = React.createRef();
+    }
+
+    render() {
+        return (
+            <ContatoContainer>
+                <SectionTitleComponent title='Entre em contato'/>
+                <FormContato>
+                    <InputSmall ref={c => this.inputName = c} type='text' name='fname' placeholder='Nome (Obrigatório)'/>
+                    <InputSmall ref={c => this.inputEmail = c} type='email' name='email' placeholder='E-mail (Obrigatório)'/>
+                    <InputSmall ref={c => this.inputSubject = c} type='text' name='subject' placeholder='Assunto'/>
+                    <InputBig ref={c => this.inputMessage = c} type='text' name='message' placeholder='Mensagem'/>
+                </FormContato>
+                <ButtonContato onClick={() => sendEmail({
+                    name: this.inputName.value,
+                    email: this.inputEmail.value,
+                    subject: this.inputSubject.value,
+                    message: this.inputMessage.value
+                })}>ENVIAR MENSAGEM</ButtonContato>
+            </ContatoContainer>
+        )
+    }
+}
 
 export default ContatoForm;
