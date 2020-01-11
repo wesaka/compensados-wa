209c4059a7993d1a99baeb2f676816b25aefd078
wesaka <kalatai.alberti@gmail.com>
2020-01-10 21:19:09 -0300

    Added the historia content to the empresa page

--- src/components/historia/historia.styles.jsx
+++ src/components/historia/historia.styles.jsx
@@ -1,1 +1,54 @@
 import styled from "styled-components";
+
+export const HistoriaContainer = styled.div`
+    display: flex;
+    align-content: center;
+    align-items: start;
+    background: white;
+    flex-wrap: wrap;
+    justify-content: space-around;
+`;
+
+export const Logo = styled.img`
+    width: 228px;
+    height: 182px;
+    max-width: 800px;
+    min-width: 228px;
+    flex: 0 0 20%;
+    padding-left: 2em;
+    padding-top: 2em;
+`;
+
+export const HistoriaTextContainer = styled.div`
+    min-width: 350px;
+    padding: 2em;
+    text-align: left;
+    flex: 1 0 20%;
+`;
+
+export const HistoriaTitleContainer = styled.div`
+    display: flex;
+    flex-wrap: nowrap;
+    flex-direction: row;
+`;
+
+export const HistoriaTitle = styled.h2`
+    flex: initial;
+    white-space: nowrap;
+`;
+
+export const HistoriaDivider = styled.hr`
+    width: 100%;
+    box-sizing: content-box;
+    height: 6px;
+    border: 1px solid #b3b3b3;
+    border-right-width: 0;
+    border-left-width: 0;
+    margin-left: 1em;
+    margin-top: auto;
+    margin-bottom: auto;
+`;
+
+export const HistoriaParagraph = styled.p`
+
+`;
--- src/pages/empresa/empresa.component.jsx
+++ src/pages/empresa/empresa.component.jsx
@@ -3,10 +3,12 @@ import ParallaxHeaderComponent from "../../components/parallax-header/parallax-h
 import {ProdutosPageContainer} from "../produtos/produtos.styles";
 import ProdutoComponent from "../../components/produto/produto.component";
 import PicturesComponent from "../../components/pictures/pictures.component";
+import HistoriaComponent from "../../components/historia/historia.component";
 
 const EmpresaPage = () => (
     <div>
         <ParallaxHeaderComponent title='Conheça a W.A.' background='/images/empresa.jpg'/>
+        <HistoriaComponent/>
         <PicturesComponent/>
     </div>
 );
